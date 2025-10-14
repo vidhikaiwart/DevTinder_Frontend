e1ba32d87096c768f515689769fd8903c90cb43f
vidhikaiwart <vidhikaiwart@gmail.com>
2025-10-14 14:35:38 +0530

    frontend setup

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+
+
+import { BrowserRouter, Routes , Route} from "react-router-dom";
+
+import Body from "./Body";
+import Login from "./login";
+import Profile from "./Profile";
+
+
+function App() {
+  
+
+  return (
+    <>
+<BrowserRouter basename="/" >
+<Routes>
+
+   <Route   path="/" element= {<Body />}>
+   <Route  path="/login" element= {<Login />}/>
+    <Route  path="/profile" element= {<Profile />}/>
+
+ </Route>
+</Routes>
+
+</BrowserRouter>
+  
+    </>
+  )
+}
+
+export default App
